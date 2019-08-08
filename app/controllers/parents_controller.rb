9cddd5fa7dec60beb05499934c6020383bf41cfa
billyliberty <joshua.crum@gmail.com>
2019-08-08 10:56:59 -0500

    Added destroy action for logging out of session.

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -23,11 +23,20 @@ class ParentsController < ApplicationController
     end
 
     def destroy
+        @parent.destroy
+        session.delete :parent_id
+        @current_parent = nil
+        redirect_to root_path
     end
 
     private
 
     def parent_params
+
+    end
+
+    def set_parent
+        @parent = Parent.find_by(id: params[:id])
     end
 
 end
