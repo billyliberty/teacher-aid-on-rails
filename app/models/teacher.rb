194a9aa1ae8c649c7d3351f8486b0b6cba063200
billyliberty <joshua.crum@gmail.com>
2019-08-08 11:10:41 -0500

    Updated models with relationships and validations.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,11 @@
 class Teacher < ApplicationRecord
+    has_secure_password
+
     has_many :events
     has_many :supply_lists
     has_many :event_shifts, through: :events
     has_many :supply_items, through: :supply_lists
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
