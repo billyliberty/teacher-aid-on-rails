a7fe4ea1fa496bcdd361ab56b875d702a8515f31
billyliberty <joshua.crum@gmail.com>
2019-08-08 11:59:56 -0500

    Stubbed out actions.

--- app/controllers/supply_lists_controller.rb
+++ app/controllers/supply_lists_controller.rb
@@ -1,2 +1,18 @@
 class SupplyListsController < ApplicationController
+
+    def index
+        @supply_list = SupplyList.new
+        @supply_lists = SupplyList.all 
+        render 'supply_lists/index.html.erb'
+    end
+
+    def show
+        @supply_list = SupplyList.find(params[:id])
+    end
+
+    def create
+    end
+
+    private
+
 end
