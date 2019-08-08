b392488a73930c661f30031da7f19e4419024f5b
billyliberty <joshua.crum@gmail.com>
2019-08-08 13:52:00 -0500

    Revised models.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class TeachersController < ApplicationController
+class UsersController < ApplicationController
 
     def new
     end
--- app/models/parent.rb
+++
@@ -1,9 +0,0 @@
-class Parent < ApplicationRecord
-    has_secure_password
-
-    has_many :supply_items
-    has_many :event_shifts
-
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-end
